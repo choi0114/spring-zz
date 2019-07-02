f61a3366e90deb7a39744cdcb8577e0f1b3d3d8b
eunjeong <tanvely0409@gmail.com>
2019-07-02 13:40:19 +0900

    마스터 병합 테스트

--- src/main/java/com/sample/test/TestController.java
+++ src/main/java/com/sample/test/TestController.java
@@ -11,4 +11,10 @@ public class TestController {
 		System.out.println("깃-소스트리 연동 테스트입니다.");
 		return "test";
 	}
+	
+	@RequestMapping("/merge.do")
+	public String merge() {
+		System.out.println("마스터-버그픽스 병합 테스트입니다.");
+		return "merge";
+	}
 }
